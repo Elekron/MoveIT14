49706a8969e9fc8c5481085d0133b746740f1722
Elekron <andreas.bolzyk@gmail.com>
2015-01-17 13:32:56 +0100

    Add android

---
+++ GreenWear/Application/src/main/java/com/example/android/horizontalpaging/LoginActivity.java
@@ -0,0 +1,59 @@
+package com.example.android.horizontalpaging;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+
+
+public class LoginActivity extends Activity {
+
+    public final static String LOGIN_MESSAGE = "com.mycompany.myfirstapp.MESSAGE";
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_login, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    /**
+     *
+     * @param view
+     */
+    public void login(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+        EditText editText = (EditText) findViewById(R.id.username_);
+        String message = editText.getText().toString();
+        intent.putExtra(LOGIN_MESSAGE, message);
+        startActivity(intent);
+    }
+
+
+}
